af5a47cd6b88af8e4d771f2e94eab98e2fcda893
Evgeny <mr_orange@inbox.ru>
2022-02-24 10:44:31 +0300

    HomeWorkLesson3-1

--- src/ru/gb/evgenygasyuta/HomeWorkApp3.java
+++ src/ru/gb/evgenygasyuta/HomeWorkApp3.java
@@ -2,6 +2,20 @@ package ru.gb.evgenygasyuta;
 
 public class HomeWorkApp3 {
     public static void main(String[] args) {
+        System.out.println("Задание 1 ");
+        changeArr();
+        System.out.println();
+        System.out.println("Задание 2");
 
+    }
+
+    private static void changeArr() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1,0,0,0,0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else arr[i] = 1;
+            System.out.print(arr[i]);
+        }
     }
 }
