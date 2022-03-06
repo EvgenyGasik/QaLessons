d5c8177f54069059c3667d3594bcb21892519093
Evgeny <mr_orange@inbox.ru>
2022-03-06 14:17:26 +0300

    HomeWork 6 -3

--- src/ru/gb/evgenygasyuta/lesson6/HomeWorkApp6.java
+++ src/ru/gb/evgenygasyuta/lesson6/HomeWorkApp6.java
@@ -2,6 +2,18 @@ package ru.gb.evgenygasyuta.lesson6;
 
 public class HomeWorkApp6 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Animals animals[] = {
+                new Cat("Tom"),
+                new Dogs("Sharik"),
+                new Dogs("Spike"),
+                new Cat("Kitty")
+
+        };
+        for (int i = 0; i < animals.length; i++) {
+            animals[i].toRun(400);
+            animals[i].toSwim(5);
+
+        }
     }
 }
