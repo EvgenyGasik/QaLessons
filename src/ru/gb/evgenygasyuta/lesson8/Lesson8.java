724c50603f059340d5607010f5f87a8316a63356
evgeny <mr_orange@inbox.ru>
2022-04-12 22:49:39 +0300

    Lesson8Done

--- src/ru/gb/evgenygasyuta/lesson8/Lesson8.java
+++ src/ru/gb/evgenygasyuta/lesson8/Lesson8.java
@@ -1,7 +1,53 @@
 package ru.gb.evgenygasyuta.lesson8;
 
-public class Lesson8 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+import javax.swing.*;
+import javax.swing.tree.VariableHeightLayoutCache;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Lesson8 extends JFrame {
+    int count = 0;
+    public Lesson8(){
+        setBounds(300,300,400,200);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Counter");
+        setResizable(false);
+
+
+        JLabel counter = new JLabel("0");
+        JButton leftButton = new JButton("-");
+        JButton rightButton = new JButton("+");
+
+        Font font = new Font("Dialog",Font.BOLD,32);
+        counter.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+        leftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count --;
+                counter.setText(String.valueOf(count));
+            }
+        });
+        rightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count ++;
+                counter.setText(String.valueOf(count));
+            }
+        });
+
+        add(leftButton, BorderLayout.EAST);
+        add(rightButton, BorderLayout.WEST);
+        add(counter, BorderLayout.CENTER);
+
+        setVisible(true);
+
     }
+
+    public static void main(String[] args) {
+        new Lesson8();
+    }
+
 }
